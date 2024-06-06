f80eb9afe69894f8d1b3eb687693e461b97349f6
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-06 22:19:27 +0100

    test

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """task 4"""
+
+
+
 a: int = 1  # Define a variable 'a' of type int and assign it the value 1
 pi: float = 3.14  # Define a variable 'pi' of type float and assign it the value 3.14
-i_understand_annotations: bool = True  # Define a variable 'i_understand_annotations' of type bool and assign it the value True
-school: str = "Holberton"  # Define a variable 'school' of type string and assign it the value "Holberton"
+# Define a variable 'i_understand_annotations' of type bool and assign it the value True
+i_understand_annotations: bool = True
+# Define a variable 'school' of type string and assign it the value "Holberton"
+school: str = "Holberton"
