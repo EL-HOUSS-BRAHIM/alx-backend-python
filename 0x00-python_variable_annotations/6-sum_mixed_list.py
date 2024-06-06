df7d85082309ed8ae66744950598c2e6fa8b524c
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-06 22:42:06 +0100

    test

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Return the sum of a list of floats and integers."""
+from typing import List
 
 
-def sum_mixed_list(mxd_lst: list[object]) -> float:
+def sum_mixed_list(mxd_lst: List[object]) -> float:
     """Return the sum of a list of floats and integers."""
     return sum(mxd_lst)
