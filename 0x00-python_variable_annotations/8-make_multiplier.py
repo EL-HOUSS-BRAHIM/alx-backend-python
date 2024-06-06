0fd543b80df66d728fa7dd1a75ca3aae98d3acae
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-06 22:46:08 +0100

    test

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Returns a function that multiplies a float by multiplier"""
+from typing import Callable
 
-
-def make_multiplier(multiplier: float) -> callable:
+def make_multiplier(multiplier: float) -> Callable:
     """Returns a function that multiplies a float by multiplier"""
     def multiply(n: float) -> float:
         return n * multiplier
