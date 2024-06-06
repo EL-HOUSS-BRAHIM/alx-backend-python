3678e2cbcf6cecfc3b1ce90f9055a9aa7608f71d
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-06 23:01:03 +0100

    test

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """This script defines a function to_kv that takes a string 
-and a number as input.."""
+and a number as input..
+"""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Converts a string and a number to a tuple containing the 
-    string and the square of the number."""
+    string and the square of the number.
+    """
     return (k, float(v**2))
