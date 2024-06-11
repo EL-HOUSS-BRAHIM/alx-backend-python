fee8d710cfe4519fbad1613b9010482ba2b2834e
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-12 00:20:16 +0100

    0.async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> AsyncGenerator[float, None, None]:
     """async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random(0, 10)
