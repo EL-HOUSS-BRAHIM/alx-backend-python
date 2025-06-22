924f13c30f379d553683432d402aa6be85a9b100
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-06-22 22:54:06 +0100

    updating: urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,15 +1,16 @@
-from rest_framework_nested import routers
-from .views import ConversationViewSet, MessageViewSet
 from django.urls import path, include
+from rest_framework import routers  # <-- includes DefaultRouter()
+from rest_framework_nested.routers import NestedDefaultRouter
+from .views import ConversationViewSet, MessageViewSet
 
+default_router = routers.DefaultRouter()
+default_router.register(r'conversations', ConversationViewSet, basename='conversation')
 
-router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
+nested_router = NestedDefaultRouter(default_router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
-conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
-# Registering messages as a nested ressource
-conversations_router.register(r'messages', MessageViewSet, basename='conversation-message')
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(conversations_router.urls)),
+    path('', include(default_router.urls)),
+    path('', include(nested_router.urls)),
 ]
+
